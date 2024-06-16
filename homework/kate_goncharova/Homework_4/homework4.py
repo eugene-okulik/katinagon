1777b0d6f0d7f8c6575a7805ba87fe694f055013
Ekaterina Goncharova <katinagon@yandex.ru>
2024-06-16 10:37:07 +0300

    Changed str to tuple

--- homework/kate_goncharova/Homework_4/homework4.py
+++ homework/kate_goncharova/Homework_4/homework4.py
@@ -6,7 +6,7 @@ my_dict = {'tuple': (100, 200, 300, 400, 500),
 print(my_dict['tuple'][-1])
 my_dict['list'].append('six')
 my_dict['list'].pop(1)
-my_dict['dict']['i am a tuple'] = 'кортеж'
+my_dict['dict'][('i am a tuple',)] = 'кортеж'
 my_dict['dict'].pop('март')
 my_dict['set'].add('f')
 my_dict['set'].pop()
