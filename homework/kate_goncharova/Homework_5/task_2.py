cc053b70a73983bc5f0eb49be71e1ebf49c7476d
Ekaterina Goncharova <katinagon@yandex.ru>
2024-06-18 14:15:32 +0300

    Updated slices with index

--- homework/kate_goncharova/Homework_5/task_2.py
+++ homework/kate_goncharova/Homework_5/task_2.py
@@ -1,6 +1,6 @@
 result_1 = 'результат операции: 42'
 result_2 = 'результат операции: 514'
 result_3 = 'результат работы программы: 9'
-print(f'Первый результат сложения: {int(result_1[result_1.index("42"):]) + 10}')
-print(f'Второй результат сложения: {int(result_2[result_2.index("514"):]) + 10}')
-print(f'Третий результат сложения: {int(result_3[result_3.index("9"):]) + 10}')
+print(f'Первый результат сложения: {int(result_1[result_1.index(":") + 1:]) + 10}')
+print(f'Второй результат сложения: {int(result_2[result_2.index(":") + 1:]) + 10}')
+print(f'Третий результат сложения: {int(result_3[result_3.index(":") + 1:]) + 10}')
