6ce45d195ee2f756307522f701d452290f0be8da
Ekaterina Goncharova <katinagon@yandex.ru>
2024-07-24 16:19:34 +0300

    Changed sort and search methods

--- homework/kate_goncharova/Homework_12/task_about_flowers.py
+++ homework/kate_goncharova/Homework_12/task_about_flowers.py
@@ -1,31 +1,35 @@
 class Flower:
-    def __init__(self, petals_color, height, freshness, lifetime, cost):
+    def __init__(self, name, petals_color, height, freshness, lifetime, cost):
+        self.name = name
         self.petals_color = petals_color
         self.height = height
         self.freshness = freshness
         self.lifetime = lifetime
         self.cost = cost
 
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return self.name
+
 
 class Rose(Flower):
-    name = 'rose'
 
-    def __init__(self, petals_color, height, freshness, lifetime, cost):
-        super().__init__(petals_color, height, freshness, lifetime, cost)
+    def __init__(self, name, petals_color, height, freshness, lifetime, cost):
+        super().__init__(name, petals_color, height, freshness, lifetime, cost)
 
 
 class Lily(Flower):
-    name = 'lily'
 
-    def __init__(self, petals_color, height, freshness, lifetime, cost):
-        super().__init__(petals_color, height, freshness, lifetime, cost)
+    def __init__(self, name, petals_color, height, freshness, lifetime, cost):
+        super().__init__(name, petals_color, height, freshness, lifetime, cost)
 
 
 class Tulip(Flower):
-    name = 'tulip'
 
-    def __init__(self, petals_color, height, freshness, lifetime, cost):
-        super().__init__(petals_color, height, freshness, lifetime, cost)
+    def __init__(self, name, petals_color, height, freshness, lifetime, cost):
+        super().__init__(name, petals_color, height, freshness, lifetime, cost)
 
 
 class Bouquet:
@@ -40,38 +44,26 @@ class Bouquet:
     def __bouquet_cost(self):
         return sum(flower.cost for flower in self.flowers)
 
-    def sorted_by_color(self, color):
-        sorted_flowers = [flower.name for flower in self.flowers if flower.petals_color == color]
-        if sorted_flowers:
-            return f"Цветы c параметром color {color} в букете: {', '.join(sorted_flowers)}"
-        else:
-            return f"В букете нет цветов с параметром color {color}"
+    def sorted_by_color(self):
+        sorted_flowers = sorted(self.flowers, key=lambda flower: flower.petals_color)
+        return f"Отсортированный список цветов по цвету: {sorted_flowers}"
 
-    def sorted_by_freshness(self, freshness):
-        sorted_flowers = [flower.name for flower in self.flowers if flower.freshness == freshness]
-        if sorted_flowers:
-            return f"Цветы c параметром freshness {freshness} в букете: {', '.join(sorted_flowers)}"
-        else:
-            return f"В букете нет цветов с параметром freshness {freshness}"
+    def sorted_by_freshness(self):
+        sorted_flowers = sorted(self.flowers, key=lambda flower: flower.freshness)
+        return f"Отсортированный список цветов по свежести: {sorted_flowers}"
 
-    def sorted_by_height(self, height):
-        sorted_flowers = [flower.name for flower in self.flowers if flower.height == height]
-        if sorted_flowers:
-            return f"Цветы c параметром height {height} в букете: {', '.join(sorted_flowers)}"
-        else:
-            return f"В букете нет цветов с параметром height {height}"
+    def sorted_by_height(self):
+        sorted_flowers = sorted(self.flowers, key=lambda flower: flower.height)
+        return f"Отсортированный список цветов по высоте: {sorted_flowers}"
 
-    def sorted_by_cost(self, cost):
-        sorted_flowers = [flower.name for flower in self.flowers if flower.cost == cost]
-        if sorted_flowers:
-            return f"Цветы c параметром cost {cost} в букете: {', '.join(sorted_flowers)}"
-        else:
-            return f"В букете нет цветов с параметром cost {cost}"
+    def sorted_by_cost(self):
+        sorted_flowers = sorted(self.flowers, key=lambda flower: flower.cost)
+        return f"Отсортированный список цветов по цене: {sorted_flowers}"
 
     def search_by_lifetime(self, lifetime):
-        sorted_flowers = [flower.name for flower in self.flowers if flower.lifetime == lifetime]
+        sorted_flowers = [flower for flower in self.flowers if flower.lifetime == lifetime]
         if sorted_flowers:
-            return f"Цветы c параметром lifetime {lifetime} в букете: {', '.join(sorted_flowers)}"
+            return f"Цветы c параметром lifetime {lifetime} в букете: {sorted_flowers}"
         else:
             return f"В букете нет цветов с параметром lifetime {lifetime}"
 
@@ -84,14 +76,14 @@ class Bouquet:
         return self.__bouquet_cost
 
 
-yellow_lily = Lily('yellow', 30, False, 10, 160)
-red_rose = Rose('red', 60, True, 12, 250)
-white_tulip = Tulip('red', 25, True, 8, 194)
+yellow_lily = Lily('yellow_lily', 'yellow', 30, False, 10, 160)
+red_rose = Rose('red_rose', 'red', 60, True, 12, 250)
+white_tulip = Tulip('white_tulip', 'white', 25, False, 8, 194)
 bouquet1 = Bouquet(yellow_lily, red_rose, white_tulip)
 print(f"Среднее время увядания букета около {bouquet1.wilting_time} дней")
 print(f"Стоимость букета {bouquet1.bouquet_cost} руб.")
-print(bouquet1.sorted_by_color('yellow'))
-print(bouquet1.sorted_by_freshness(True))
-print(bouquet1.sorted_by_height(25))
-print(bouquet1.sorted_by_cost(250))
+print(bouquet1.sorted_by_color())
+print(bouquet1.sorted_by_freshness())
+print(bouquet1.sorted_by_height())
+print(bouquet1.sorted_by_cost())
 print(bouquet1.search_by_lifetime(10))
